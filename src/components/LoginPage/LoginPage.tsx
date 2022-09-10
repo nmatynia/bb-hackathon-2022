01d9051184c3f9387a9279066da701aa43413611
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-09-11 00:55:32 +0200

    Almost finished the login page

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -2,32 +2,56 @@ import React from 'react'
 import { NavBar } from './NavBar'
 import { Input } from '@chakra-ui/react'
 import { Button } from '@chakra-ui/react'
-import { Text } from '@chakra-ui/react'
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+} from '@chakra-ui/react'
+
+import { useState } from 'react'
 
 export const LoginPage = () => {
-  return (
-    <div className='flex flex-col bg-gradient-to-b from-blue-900 to-neutral-700 overflow-hidden h-[100vh]'>
-        <NavBar/>
-        <div className='flex justify-end w-[1280px] h-full'>
-            <div className='flex my-10'>
 
-                <div className='text-3xl text-white font-bold'>
-                    Join <span className='bg-teal-500 rounded-md px-2 py-1'>Lorem</span> now!
-                </div>
-                
-                <div className='flex flex-col justify-center bg-gradient-to-b from-white to-neutral-100 w-[400px] h-[500px] rounded-lg'>
-                    <div className='flex flex-col items-center'>
-                        <Input width='300px' color='white' placeholder='Login' className='mb-5'/>
-                        <Input width='300px' placeholder='Password' className='mb-5' />
-                        <Button width='300px' colorScheme='teal' className='mb-5' >Join</Button>
-                        <p className='font-bold mb-5'>or</p>
-                        <Button width='300px' colorScheme='blue' className='mb-5'>Facebook</Button>
-                        <Button width='300px' variant='outline' colorScheme='black' className=''>Google</Button>
+    const [login, setLogin] = useState('');
+    const [password,setPassword] = useState('');
+
+    const handleLoginChange = (e:any) => setLogin(e.target.value);
+    const handlePasswordChange = (e:any) => setPassword(e.target.value);
+   
+    //TODO: Error handeling 
+    const isError = false;
+    return (
+        <div className='flex flex-col bg-gradient-to-b from-blue-900 to-neutral-700 overflow-hidden h-[100vh]'>
+            <NavBar />
+            <div className='flex justify-around h-full'>
+                <div className='flex my-10'>
+
+                    <div className='text-white mr-60'>
+                       <div className='text-6xl text-white font-bold'>Join <span className='bg-teal-500 rounded-md px-2 py-1'>Lorem</span> now!</div>
+                       <div className='text-4xl text-white mt-5'>Best way to have insight <br/> in your energy source.</div>
                     </div>
-                </div>
 
+                    <div className='flex flex-col justify-center bg-gradient-to-b from-white to-neutral-100 w-[400px] h-[500px] rounded-lg shadow-xl'>
+                        <div className='flex flex-col items-center space-y-5'>
+                            {isError 
+                                ?<Input isInvalid width='300px' placeholder='Login' className='' value={login} onChange={handleLoginChange} focusBorderColor='teal.500'/> 
+                                : <Input width='300px' placeholder='Login' className='' value={login} onChange={handleLoginChange} focusBorderColor='teal.500'/>
+                            }
+                            {isError 
+                                ?<Input isInvalid width='300px' placeholder='Password' className='' value={password} onChange={handlePasswordChange} focusBorderColor='teal.500'/> 
+                                : <Input width='300px' placeholder='Password' className='' value={password} onChange={handlePasswordChange} focusBorderColor='teal.500'/>
+                            }
+                            <Button width='300px' colorScheme='teal' className='' >Login</Button>
+                            <p className='font-bold mb-5'>or</p>
+                            <Button width='300px' colorScheme='blue' className=''>Facebook</Button>
+                            <Button width='300px' variant='outline' colorScheme='black' className=''>Google</Button>
+                            <Button width='300px' colorScheme='teal' className='' >Join us</Button>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
--- src/components/LoginPage/NavBar.tsx
+++ src/components/LoginPage/NavBar.tsx
@@ -4,7 +4,7 @@ import { Button } from '@chakra-ui/react'
 export const NavBar = () => {
       
   return (
-    <div className='flex items-center justify-between w-[100% - 128px] mx-16 mt-3 rounded-lg h-16 bg-slate-100'>
+    <div className='flex items-center justify-between w-[100% - 128px] mx-16 mt-3 rounded-lg h-16 bg-gradient-to-b from-white to-neutral-100 shadow-2xl'>
         <h1 className='font-bold text-2xl text-neutral-800 ml-7'>Lorem</h1>
         <div className='mr-7 space-x-6'>
             <Button colorScheme='teal' variant='outline'>Login</Button>
