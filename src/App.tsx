dabd726dcc84257deb3363a645ab1871f04929ed
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-09-10 19:51:05 +0200

    Routing and temporary login handeling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { LoginPage }  from './components/LoginPage/LoginPage'
+import { MainPage } from './components/LoginPage/MainPage/MainPage';
 
 const App = () => {
+  const loggedIn = true;
+
   return (
     <ChakraProvider>
-      <div className="App">
-      </div>
+      <Router>
+        <Switch>
+        {/* If loggedIn  show LoginPage if not MainPage*/}
+            <Route path='/login'>
+              <LoginPage/>
+            </Route>
+            <Route exact path='/'>
+              {loggedIn ? <Redirect to ="/login"/>:<MainPage/>}
+            </Route>
+        </Switch>
+      </Router>
     </ChakraProvider>
   );
 }
